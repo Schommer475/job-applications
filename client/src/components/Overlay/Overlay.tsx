e873f02f15a92691734593f30627e6a41ad01151
tim.schommer <schommer7.tim@gmail.com>
2026-05-30 10:52:09 -0500

    feat: created generic, extensible overlay component

---
+++ client/src/components/Overlay/Overlay.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import "./Overlay.css";
+
+export default function Overlay ({
+	showing,
+	className,
+	overlayContent,
+	contentProps = {},
+	children,
+	...overlayProps
+}: OverlayProps) {
+	const classNames = ["overlay"],
+		contentClassNames = ["overlay-content"],
+		{className: contentClassName, ...remainingContentProps} = contentProps;
+
+	if (className) {
+		classNames.push(className);
+	}
+
+	if (contentClassName) {
+		contentClassNames.push(contentClassName);
+	}
+
+	return (
+		<div
+			className={classNames.join(" ")}
+			{...overlayProps}
+		>
+			{showing && (
+				<div className="overlay-boundary">
+					<div
+						className={contentClassNames.join(" ")}
+						{...remainingContentProps}
+					>
+						{overlayContent}
+					</div>
+				</div>
+			)}
+			<div
+				className="overlay-children"
+				inert={showing || undefined}
+			>
+				{children}
+			</div>
+		</div>
+	);
+}
+
+type OverlayProps = React.HTMLAttributes<HTMLDivElement> & {
+	showing: boolean,
+	overlayContent: React.ReactNode,
+	contentProps?: Omit<React.HTMLAttributes<HTMLDivElement>, "children">,
+	children: React.ReactNode
+};
