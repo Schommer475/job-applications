968f065d2b13645c70207bf907a6ea33f2e44adc
tim.schommer <schommer7.tim@gmail.com>
2026-05-30 12:13:06 -0500

    refactor: renamed 'LoadingOverlay' to 'PendingOverlay' and leveraged the 'Overlay' base component

---
+++ client/src/components/PendingOverlay/PendingOverlay.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Overlay from "../Overlay";
+import "./PendingOverlay.css";
+
+export default function PendingOverlay ({
+	showing,
+	message = "Loading, please wait",
+	children
+}: PendingOverlayProps) {
+	return (
+		<Overlay
+			showing={showing}
+			className="pending-overlay"
+			overlayContent={(
+				<>
+					<div className="spinner" />
+					<p>
+						{message}
+					</p>
+				</>
+			)}
+		>
+			{children}
+		</Overlay>
+	);
+}
+
+type PendingOverlayProps = {
+	showing: boolean,
+	message?: string,
+	children: React.ReactNode
+};
