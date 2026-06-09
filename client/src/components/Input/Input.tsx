ad8889ec12d4d14cb49a8a0f7065c6c6768720b1
tim.schommer <schommer7.tim@gmail.com>
2026-06-08 21:08:04 -0500

    feat: input component leveraging the field wrapper

---
+++ client/src/components/Input/Input.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import FieldWrapper from "../FieldWrapper";
+import type {FieldWrapperProps} from "../FieldWrapper";
+
+export default function Input ({
+	label,
+	required,
+	error,
+	className,
+	"aria-describedby": describedBy,
+	...inputProperties
+}: InputProps) {
+	return (
+		<FieldWrapper
+			label={label}
+			required={required}
+			error={error}
+			className={className}
+		>
+			{(id: string, describerId: string) => (
+				<input
+					id={id}
+					className="field-input"
+					required={required}
+					aria-invalid={Boolean(error)}
+					aria-describedby={[describerId, describedBy].filter(Boolean).join(" ")}
+					type="text"
+					{...inputProperties}
+				/>
+			)}
+		</FieldWrapper>
+	);
+}
+
+type InputProps = Omit<FieldWrapperProps, "children"> &
+	Omit<React.ComponentProps<"input">, "id" | "type">;
